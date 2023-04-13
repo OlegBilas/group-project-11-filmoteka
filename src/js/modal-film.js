405626b26976b6d74838f56b0954a612f0dcba43
Oakmoater <dubrovecva@gmail.com>
2023-04-13 22:47:51 +0200

    js_modal_sample_logic

---
+++ src/js/modal-film.js
@@ -0,0 +1,36 @@
+// Запит на сервер (функція від Шаді)
+// fetch()
+  .then(response => response.json())
+  .then(data => {
+    // Розпарсинг даних
+    const results = data.results;
+    const movie = results[0];
+    const title = movie.title;
+    const voteAverage = movie.vote_average;
+    const voteCount = movie.vote_count;
+    const popularity = movie.popularity;
+    const releaseDate = movie.release_date;
+    const overview = movie.overview;
+
+    // Відображення даних на сторінці згідно розмітки Олександра
+    const titleElement = document.querySelector('.form__title');
+    titleElement.textContent = title;
+
+    const ratingElement = document.querySelector('.film__rating');
+    ratingElement.textContent = voteAverage;
+
+    const voteCountElement = document.querySelector('.form__info-details span:nth-child(2)');
+    voteCountElement.textContent = voteCount;
+
+    const popularityElement = document.querySelector('.form__info-details:nth-child(4)');
+    popularityElement.textContent = popularity;
+
+    const releaseDateElement = document.querySelector('.form__info-details:nth-child(5)');
+    releaseDateElement.textContent = releaseDate;
+
+    const overviewElement = document.querySelector('.form__about-details');
+    overviewElement.textContent = overview;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
