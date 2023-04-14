8b9bdc9a2cc501f110712c285f00c123f4b4cd3b
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-04-14 17:17:50 +0300

    functions addToLocalstorage and getFromLocalstorage
    created functions to save and get objects to local storage

---
+++ src/js/localAPI.js
@@ -0,0 +1,15 @@
+export function addToLocalstorage(key, dataAboutFilm) {
+    const arrayByKey = getFilmsFromLocalstorage(key);
+
+    if (arrayByKey.some(obj => obj.id === dataAboutFilm.id)) {
+        return;
+    }
+
+    const data = JSON.stringify(dataAboutFilm);
+    localStorage.setItem(`${key}`, data);
+}
+
+export function getFromLocalstorage(key) {
+        const dataJSON =  localStorage.getItem(`${key}`);
+        return JSON.parse(dataJSON);
+}
