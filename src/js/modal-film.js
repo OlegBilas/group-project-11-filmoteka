7526820abef0958e63eb6228b4016b1defac166c
Oakmoater <dubrovecva@gmail.com>
2023-04-15 09:16:23 +0200

    added_video_to_murkup

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -1,6 +1,9 @@
 import * as basicLightbox from 'basiclightbox'
+import { YoutubeVideo } from './youtubevideo';
 
 export function renderMovieModal({ genres, id, poster_path, original_title, overview, popularity, vote_average, vote_count, videoId }) {
+  const youtubeVideo = new YoutubeVideo();
+  const videoIframe = youtubeVideo.createIframe(videoId);
 
   const instance = basicLightbox.create(`
 <div class="backdrop js-backdrop">
@@ -11,48 +14,48 @@ export function renderMovieModal({ genres, id, poster_path, original_title, over
                 <use href="/images/sprite.svg#icon-close"></use>
             </svg> </button>
         <div id="modal_form">
-            ​
-            <form class="form">
-                <div class="form__film-card">
-                    <div class="form__film-img">
-                        <img class="form-img" src="https://image.tmdb.org/t/p/w400/${poster_path}"
-                            alt="${original_title}">
-                    </div>
+          <form class="form">
+              <div class="form__film-card">
+                  <div class="form__film-img">
+                      <img class="form-img" src="${poster_path}"
+                          alt="${original_title}">
+                  </div>
+              </div>
+              <div class="form__info">
+                  <h2 class="form__title">${original_title}</h2>
+                  <ul class="form__info-list">
+                      <li class="form__info-item">
+                          <p class="form__info-name">Vote / Votes</p>
+                          <p class="form__info-details"> <span class="film__rating">${vote_average}</span>/<span class="film__voice">${vote_count}</span></p>
+                      </li>
+                      <li class="form__info-item">
+                          <p class="form__info-name">Popularity</p>
+                          <p class="form__info-details">${popularity}</p>
+                      </li>
+                      <li class="form__info-item">
+                          <p class="form__info-name">Original Title</p>
+                          <p class="form__info-details details-title">${original_title}</p>
+                      </li>
+                      <li class="form__info-item">
+                          <p class="form__info-name">Genre</p>
+                          <p class="form__info-details">${genres}</p>
+                      </li>
+                  </ul>
+                  <div class="form__about">
+                      <h3 class="form__about-name">About</h3>
+                      <p class="form__about-details">${overview}</p>
+                  </div>
+                  <div class="form__btn-list">
+                      <button data-action="add" id="watched" class="form-button form-add-btn" type="button">Add to watched</button>
+                      <button data-action="add" id="queue" class="form-button form-queue-btn" type="button">Add to queue</button>
+                  </div>
+              </div>
+              <div class="form__film-card">
+                <div class="form__film-img">
+                ${videoIframe.outerHTML}
                 </div>
-                <div class="form__info">
-                    <h2 class="form__title">${original_title}</h2>
-                    <ul class="form__info-list">
-                        <li class="form__info-item">
-                            <p class="form__info-name">Vote / Votes</p>
-                            <p class="form__info-details"> <span class="film__rating">${vote_average}
-                                    ​
-                                </span>/<span class="film__voice">${vote_count}</span></p>
-                        </li>
-                        <li class="form__info-item">
-                            <p class="form__info-name">Popularity</p>
-                            <p class="form__info-details">${popularity}</p>
-                        </li>
-                        <li class="form__info-item">
-                            <p class="form__info-name">Original Title</p>
-                            <p class="form__info-details details-title">${original_title}</p>
-                        </li>
-                        <li class="form__info-item">
-                            <p class="form__info-name">Genre</p>
-                            <p class="form__info-details">${genres}</p>
-                        </li>
-                    </ul>
-                    <div class="form__about">
-                        <h3 class="form__about-name">About</h3>
-                        <p class="form__about-details">${overview}</p>
-                    </div>
-                    <div class="form__btn-list">
-                        <button data-action="add" id="watched" class="form-button form-add-btn" type="button">Add to
-                            watched</button>
-                        <button data-action="add" id="queue" class="form-button form-queue-btn" type="button">Add to
-                            queue</button>
-                    </div>
-                </div>
-            </form>
+              </div>
+          </form>
         </div>
     </div>
 </div>
