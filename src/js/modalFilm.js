b00bd32c0ccd7ed600b9568bfef1cd774f76868b
Fransua Boltof <bolt2000@ukr.net>
2023-04-15 11:24:31 +0300

    +fix css

---
+++ src/js/modalFilm.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-open-modal]'),
+    closeModalBtn: document.querySelector('[data-close-modal]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
