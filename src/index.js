3dfcdeaa53b11c582e2cab3916ab1cb1d9eebc62
NatiRumi <NatiRumiantseva@gmail.com>
2023-04-15 11:42:01 +0300

    style2

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@
 
 //Приклад підключення файлу скрипта
 //import { Filmoteka } from './scripts/fetch-api';
-import {renderGallery} from './js/renderGallery';
+import {templateGallery} from './js/templateGallery';
