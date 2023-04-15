75f9ad5094d4f6a68e5c120ebbcc39d25e7a5c97
InnaShavarovska <innashavarovska@gmail.com>
2023-04-15 12:12:53 +0300

    +скрипт модалки

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //Приклад підключення бібліотеки
 //import Notiflix from 'notiflix';
-import * as basicLightbox from 'basiclightbox'
+
 //Приклад підключення файлу скрипта
 //import { Filmoteka } from './scripts/fetchAPI';
-import { footerBtn, onClick } from "./js/modal-team";
+ import modal from './js/modal';
