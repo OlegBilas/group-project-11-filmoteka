c7bc3876eae3ab87108095bf5e201146f3ebd7f4
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-04-15 23:19:08 +0300

    create function to create pagination

---
+++ src/js/pagination.js
@@ -0,0 +1,41 @@
+import Pagination from 'tui-pagination';
+import { fetchFilms } from "./fetchAPI";
+import { renderCollection } from "./renderGallery";
+
+const galleryList = document.querySelector('.list');
+const container = document.querySelector(".pagination");
+function createPagination(totalItems, filmName){
+    let instance = new Pagination(container, {
+        totalItems,
+        itemsPerPage: 1,
+        visiblePages: 10,
+        page: 1,
+        centerAlign: true,
+        firstItemClassName: 'tui-first-child',
+        lastItemClassName: 'tui-last-child',
+        template: {
+            page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+            currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+            moveButton:
+                '<a href="#" class="tui-page-btn tui-{{type}}">' +
+                '<span class="tui-ico-{{type}}">{{type}}</span>' +
+                '</a>',
+            disabledMoveButton:
+                '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+                '<span class="tui-ico-{{type}}">{{type}}</span>' +
+                '</span>',
+            moreButton:
+                '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+                '<span class="tui-ico-ellip">...</span>' +
+                '</a>'
+        }
+    });
+    
+    instance.on('afterMove', (event) => {
+        const currentPage = event.page;
+        [...galleryList.children].forEach(element => element.remove());
+        renderCollection(fetchFilms(currentPage, filmName));
+    });
+}
+
+export {createPagination}
