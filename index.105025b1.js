bc0ad5a449202762d13c3e6acc93547579c701d7
OlegBilas <114345352+OlegBilas@users.noreply.github.com>
2023-04-16 05:19:41 +0000

    Deploying to gh-pages from @ OlegBilas/group-project-11-filmoteka@c52b44d76d9a2499cadf273955fe292b956e9663 🚀

--- index.105025b1.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-open-modal]"),closeModalBtn:document.querySelector("[data-close-modal]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.105025b1.js.map
--- index.a1f4b865.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-open-modal]"),closeModalBtn:document.querySelector("[data-close-modal]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.a1f4b865.js.map
