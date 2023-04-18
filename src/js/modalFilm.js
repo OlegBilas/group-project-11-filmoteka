278ba23b4bcdbe45bdbba27feb58a648f1a39fbf
Fransua Boltof <bolt2000@ukr.net>
2023-04-18 17:02:46 +0300

    Update modalFilm.js

--- src/js/modalFilm.js
+++ src/js/modalFilm.js
@@ -60,7 +60,7 @@ export function renderMovieModal(movieData, objectCard) {
       <div class="modal" data-modal="2">
         <button type="button" class="btn-close js-close-modal" id="btn-close">
             <svg class="form__close-icon" width="24px" height="24px" viewBox="0 0 30 30">
-<use data-btn="close" xlink:href="./src/images/icons.svg#icon-close"> </use>
+<use data-btn="close" xlink:href="./src/images/icons.svg#icon-close" />
     <path data-btn="close" d="M14.734 10.304l-1.504-1.504-5.963 5.962-5.962-5.962-1.504 1.504 5.962 5.962-5.962 5.963 1.504 1.504 5.962-5.963 5.963 5.963 1.504-1.504-5.963-5.963 5.963-5.962z"></path>
 </symbol>
     </svg>
